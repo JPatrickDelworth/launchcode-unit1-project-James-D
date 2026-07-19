aaee02b85871cdb6d574607193f5fc6542f265a9
J Patrick Delworth <jpdelworth@gmail.com>
2026-07-18 22:48:39 -0500

    Eighteenth commit. Added form functionality on the About Me page. Users can now post comments. Utilized local storage so comments will persist. Altered some visuals for a cleaner look but otherwise styling on the entire site is still basic.

--- src/components/pages/about/AboutPage.jsx
+++ src/components/pages/about/AboutPage.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Footer from '../../layout/Footer'
 import logo from '../../../assets/logos/FFXIV Logo V3.png'
 import './AboutMe.css'
@@ -12,7 +13,76 @@ import './AboutMe.css'
 
 
 function About () {
-    
+    const [comments, setComments] = useState([
+        {
+            name: "Heironymus Bosch",
+            email: "hgbosch@yesThisIsPurgatory.com",
+            comment: "Your quaint little attempt at a website has temporarily quelled the horrific visage of the netherworld that plagues my canvas.",
+            date: "November 14, 1485  • 3:07 AM"
+        },
+        {
+            name: "The McGriddle",
+            email: "breakfastbitch@imluvinit.com",
+            comment: "Eat me, and know heartburn.",
+            date: "July 18, 2026  • 10:17 PM"
+        }
+    ]);
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        comment: ""
+    })
+
+    useEffect(() => {
+        const savedComments = localStorage.getItem("comments");
+        if (savedComments) {
+            setComments(JSON.parse(savedComments));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("comments", JSON.stringify(comments));
+    }, [comments])
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (
+            formData.name.trim() === "" || formData.email.trim() === "" || formData.comment.trim() === ""
+        ) {
+            return;
+        }
+        const now = new Date();
+        
+        const currentDate = 
+            now.toLocaleDateString("en-US", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+            }) + " • " +
+            now.toLocaleTimeString ("en-US", {
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true
+
+            })
+
+        setComments ([
+            {
+                name: formData.name,
+                email: formData.email,
+                comment: formData.comment,
+                date: currentDate
+            },
+            ...comments
+        ]);
+        setFormData ({
+            name: "",
+            email: "",
+            comment: ""
+        })
+    }
+
     return (
         <div id="about-page">
             
@@ -44,13 +114,54 @@ function About () {
                 </div>
                 <div id="about-gap5" />
                 <div id="form-container">
-                    <form className="form">
+                    <form className="form" onSubmit={handleSubmit}>
                         <div id="id-form-container">
-                            <input type="text" id="name" name="name" placeholder="Name" required />
-                            <input type="email" id="email" name="email" placeholder="Email" required />
+                            <input 
+                                type="text" 
+                                id="name" 
+                                name="name" 
+                                placeholder="Name" 
+                                value={formData.name}
+                                onChange={(event) => {
+                                    setFormData({
+                                        ...formData,
+                                        name: event.target.value
+                                    });
+                                }} 
+                                required 
+                            />
+                            <input 
+                                type="email" 
+                                id="email" 
+                                name="email" 
+                                placeholder="Email"
+                                value={formData.email} 
+                                onChange={(event) => {
+                                    setFormData({
+                                        ...formData,
+                                        email: event.target.value
+                                    })
+                                }}
+                                required 
+                            />
                         </div>
                         <div id="form-gap1" />
-                        <textarea id="message" name="message" rows="10" cols="30" placeholder="Comments..." required ></textarea>
+                        <textarea 
+                            id="message" 
+                            name="message" 
+                            rows="10" 
+                            cols="30" 
+                            placeholder="Comments..."
+                            value={formData.comment}
+                            onChange={(event) => {
+                                setFormData({
+                                    ...formData,
+                                    comment: event.target.value
+                                })
+                            }}   
+                            required 
+                        >
+                        </textarea>
                         <div id="form-gap2" />
                         <div id="submit">
                             <input id="submit-button" type="submit" value="Submit" />
@@ -62,7 +173,15 @@ function About () {
                     <div className="title">
                         Visitor Comments
                     </div>
-                    <textarea id="comment" name="comment" rows="40" cols="300"></textarea>
+                    <div id="comments-list">
+                        {comments.map((comment, index) => (
+                            <div className="comment-card" key={index}>
+                                <h4>{comment.name}</h4>
+                                <small>{comment.date}</small>
+                                <p>{comment.comment}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
                 <div id="about-gap7" />
             </div>
