7503cab9d9de7f4b08380ba2697e072326985b24
J Patrick Delworth <jpdelworth@gmail.com>
2026-07-19 16:33:12 -0500

    Twenty first commit. Added About Me text to About Me section.

--- src/components/pages/about/AboutPage.jsx
+++ src/components/pages/about/AboutPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { aboutMe } from '../../data/aboutme'
 import Footer from '../../layout/Footer'
 import logo from '../../../assets/logos/FFXIV Logo V3.png'
 import './AboutMe.css'
@@ -90,88 +91,85 @@ function About () {
         <div id="about-page">
             
             <div id="about-container">
-                <div id="about-gap1" />
                 <div id="about-logo-container">
                     <img src={logo} id="logo" alt="Final Fantasy XIV Title Logo" />
                 </div>
-                <div id="about-gap2" />
-                <div id="title-container">
-                    <div className="title">
-                        About Me
+                <div id="about-info-container">
+                    <div id="title-container">
+                        <div className="title">
+                            About Me
+                        </div>
                     </div>
-                </div>
-                <div id="about-gap3" />
-                <div id="text-container">
-                    <div id="text-box">
-                        <div id="text">
-                            About me information...
+                    <div id="text-container">
+                        <div id="text-box">
+                            <div id="text">
+                                <p id="about-text">{aboutMe}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div id="about-gap4" />
-                <div id="comment-title-container">
-                    <div className="title">
-                        Visitor Reply Submission Form
+                <div id="form-box-container">
 
+                    <div id="comment-title-container">
+                        <div className="title">
+                            Visitor Reply Submission Form
+
+                        </div>
+                    </div>
+                    <div id="form-container">
+                        <form className="form" onSubmit={handleSubmit}>
+                            <div id="id-form-container">
+                                <input 
+                                    type="text" 
+                                    id="name" 
+                                    name="name" 
+                                    placeholder="Name" 
+                                    value={formData.name}
+                                    onChange={(event) => {
+                                        setFormData({
+                                            ...formData,
+                                            name: event.target.value
+                                        });
+                                    }} 
+                                    required 
+                                />
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    name="email" 
+                                    placeholder="Email"
+                                    value={formData.email} 
+                                    onChange={(event) => {
+                                        setFormData({
+                                            ...formData,
+                                            email: event.target.value
+                                        })
+                                    }}
+                                    required 
+                                />
+                            </div>
+                            <textarea 
+                                id="message" 
+                                name="message" 
+                                rows="10" 
+                                cols="30" 
+                                placeholder="Comments..."
+                                value={formData.comment}
+                                onChange={(event) => {
+                                    setFormData({
+                                        ...formData,
+                                        comment: event.target.value
+                                    })
+                                }}   
+                                required 
+                            >
+                            </textarea>
+                            <div id="submit">
+                                <input id="submit-button" type="submit" value="Submit" />
+                            </div>
+                        </form>  
                     </div>
                 </div>
-                <div id="about-gap5" />
-                <div id="form-container">
-                    <form className="form" onSubmit={handleSubmit}>
-                        <div id="id-form-container">
-                            <input 
-                                type="text" 
-                                id="name" 
-                                name="name" 
-                                placeholder="Name" 
-                                value={formData.name}
-                                onChange={(event) => {
-                                    setFormData({
-                                        ...formData,
-                                        name: event.target.value
-                                    });
-                                }} 
-                                required 
-                            />
-                            <input 
-                                type="email" 
-                                id="email" 
-                                name="email" 
-                                placeholder="Email"
-                                value={formData.email} 
-                                onChange={(event) => {
-                                    setFormData({
-                                        ...formData,
-                                        email: event.target.value
-                                    })
-                                }}
-                                required 
-                            />
-                        </div>
-                        <div id="form-gap1" />
-                        <textarea 
-                            id="message" 
-                            name="message" 
-                            rows="10" 
-                            cols="30" 
-                            placeholder="Comments..."
-                            value={formData.comment}
-                            onChange={(event) => {
-                                setFormData({
-                                    ...formData,
-                                    comment: event.target.value
-                                })
-                            }}   
-                            required 
-                        >
-                        </textarea>
-                        <div id="form-gap2" />
-                        <div id="submit">
-                            <input id="submit-button" type="submit" value="Submit" />
-                        </div>
-                    </form>  
-                </div>
-                <div id="about-gap6" />
                 <div id="comment-container">
                     <div className="title">
                         Visitor Comments
@@ -181,12 +179,11 @@ function About () {
                             <div className="comment-card" key={index}>
                                 <h4>{comment.name}</h4>
                                 <small>{comment.date}</small>
-                                <p>{comment.comment}</p>
+                                <p className="comment-text-data">{comment.comment}</p>
                             </div>
                         ))}
                     </div>
                 </div>
-                <div id="about-gap7" />
             </div>
             <Footer />
 
